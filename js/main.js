81e8339dbd1d732c35ca32b6c5f61e5983e4a2b0
Jonas Haefele <jonas@decoded.com>
2017-06-22 18:15:27 +0100

    finsih copy, add waypoints

--- js/main.js
+++ js/main.js
@@ -7,4 +7,39 @@ $(function(){
     e.preventDefault();
     $('body').removeClass('social-active')
   })
+
+  var coCreating = new Waypoint.Inview({
+    context: $('.scroller')[0],
+    element: $('#co-creating')[0],
+    enter: function(direction) {
+      $('.title.co-creating').addClass('active')
+    },
+    exited: function(direction) {
+      $('.title.co-creating').removeClass('active')
+    }
+  })
+
+  var mindful = new Waypoint.Inview({
+    context: $('.scroller')[0],
+    element: $('#mindful')[0],
+    enter: function(direction) {
+      $('.title.mindful').addClass('active')
+    },
+    exited: function(direction) {
+      $('.title.mindful').removeClass('active')
+    }
+  })
+
+  var experiences = new Waypoint.Inview({
+    context: $('.scroller')[0],
+    element: $('#experiences')[0],
+    enter: function(direction) {
+      $('.title.experiences').addClass('active')
+    },
+    exited: function(direction) {
+      if (direction == 'up') {
+        $('.title.experiences').removeClass('active')
+      }
+    }
+  })
 })
